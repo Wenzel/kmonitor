16c08743cd89dc80bfc9bde6c8f10faac167571f
Mathieu Tarral <mathieu.tarral@gmail.com>
2016-01-12 19:31:07 +0100

    add qml gui

---
+++ src/kmonitor.cpp
@@ -0,0 +1,26 @@
+#include <QGuiApplication>
+#include <QQmlContext>
+#include <QQmlApplicationEngine>
+
+#include "processmodel.h"
+#include "socketunixmodel.h"
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    ProcessModel process_model;
+    SocketUNIXModel socket_unix_model;
+
+    QQmlApplicationEngine engine;
+
+    QQmlContext* ctxt = engine.rootContext();
+    ctxt->setContextProperty("process_model", &process_model);
+    ctxt->setContextProperty("socket_unix_model", &socket_unix_model);
+
+    engine.load(QUrl(QStringLiteral("qrc:/kmonitor.qml")));
+
+
+
+    return app.exec();
+}
